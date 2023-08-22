2cf701543390c8e4a826ca0afa53a72c978532cf
Evgeniy Zinchenko <JjjackVat@gmail.com>
2023-08-22 18:39:02 +0300

    Управление схемами баз данных 1

--- src/main/java/ru/hogwarts/school/scripts423.sql
+++ src/main/java/ru/hogwarts/school/scripts423.sql
@@ -1,3 +1,3 @@
 SELECT student.name, student.age, faculty.name FROM student INNER JOIN faculty ON student.faculty_id = faculty.id;
 
-SELECT student.name FROM student INNER JOIN avatar a on student.id = a.student_id;
+SELECT student.name FROM student RIGHT JOIN avatar a on student.id = a.student_id;
